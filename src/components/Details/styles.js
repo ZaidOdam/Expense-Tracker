08fd2fdb46f440e9bc2728c43afbe4da9a2d1da9
Neha <patilneha0206@gmail.com>
2022-04-15 20:35:30 +0530

    Added

---
+++ src/components/Details/styles.js
@@ -0,0 +1,15 @@
+import { makeStyles } from "@material-ui/core";
+
+export default makeStyles(()=>({
+    income:{
+        borderBottom:'10px solid rgba(0,255,0,0.5)',
+    },
+    expense:{
+        borderBottom:'10px solid rgba(255,0,0,0.5)',
+    }
+
+}));
+// moti moti akhiyon se tu mujhko hi to tadde
+//dek menu lagta sar phira, manna mere surut ki hain chaandi so takka sonee,dil mera ye tuzpe mar mita
+//ye meri chaand baliyaan dikha ke  tuzko paa liyaa x3
+// sochne ka mauka hai diya abse tu hai mera hi piya
---
+++ src/context/context.js
@@ -0,0 +1,25 @@
+// implement logic for react context api
+
+import React, { useReducer, createContext } from 'react';
+import contextReducer from './contextReducer';
+const initialState=[];
+export const ExpenseTrackerContext = createContext(initialState);
+
+export const Provider = ({ children }) => {
+  const [transactions, dispatch] = useReducer(contextReducer, initialState);
+
+  //action creator 
+  const deleteTransaction=(id)=>{ dispatch({type:'DELETE_TRANSACTION',payload:id});}
+
+  const addTransaction=(transaction)=>{ dispatch({type:'ADD_TRANSACTION',payload:transaction});}
+  console.log(transactions);
+    return (
+      <ExpenseTrackerContext.Provider value={{
+        deleteTransaction,
+        addTransaction,
+        transactions
+        }}>
+        {children}
+      </ExpenseTrackerContext.Provider>
+    )
+  }
